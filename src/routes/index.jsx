880c1da1a4f6311455d7a79003151a36b5c7bb60
Thomas <50733945+ThomasHillebrink@users.noreply.github.com>
2025-02-06 21:16:33 +0200

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import AdminSessionEditor from "../components/AdminPanel/SessionEditor";
 // import SessionScreen from "../components/MainHackingScreen/SessionScreen";
 // import PuzzleScreen from "../components/Puzzle/PuzzleScreen";
@@ -7,34 +7,42 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Lazy imports to account for spotty internet connection
 const AdminPanelLayout = lazy(() => import('../components/AdminPanel/AdminPanelLayout'));
-const SessionScreen = lazy(() => import("../components/MainHackingScreen/MainHackingScreen"));
-const PuzzleScreen = lazy(() => import("../components/Puzzle/PuzzleScreen"));
-
-
-
+const SessionScreen = lazy(() => import('../components/MainHackingScreen/MainHackingScreen'));
+const PuzzleScreen = lazy(() => import('../components/Puzzle/PuzzleScreen'));
+const Testing = lazy(() => import('../components/test'));
+const QuickHackScreen = lazy(() => import('../components/UnplannedPuzzle/QuickHackScreen'))
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-        <Suspense fallback={<div>Establishing connection...</div>}>
-            <Routes>
-                {/* Admin Panel */}
-                <Route path="/admin" element={<AdminPanelLayout />} />
-                {/* <Route path="/admin/sessions/:sessionId" element={<AdminSessionEditor />} /> */}
+      <Suspense fallback={<div>Establishing connection...</div>}>
+        <Routes>
+          {/* Admin Panel */}
+          <Route path="/admin" element={<AdminPanelLayout />} />
+          {/* <Route path="/admin/sessions/:sessionId" element={<AdminSessionEditor />} /> */}
 
-                {/* Main Hacking Screen */}
-                <Route path="/session/:sessionId" element={<SessionScreen />} />
+          {/* Main Hacking Screen */}
+          <Route path="/session/:sessionId" element={<SessionScreen />} />
 
-                {/* Puzzle Screen */}
-                <Route path="/puzzle/:sessionId/:layerId" element={<PuzzleScreen />} />
+          {/* Puzzle Screen */}
+          <Route path="/puzzle/:sessionId/:layerId" element={<PuzzleScreen />} />
 
-                {/* Unplanned Puzzle */}
-                {/* <Route path="/puzzle/unplanned/:difficulty" element={<UnplannedPuzzleScreen />} /> */}
+          {/* Puzzle Screen */}
+          <Route path="/test" element={<Testing />} />
 
-                {/* Fallback or home route */}
-                <Route path="*" element={<div>Not Found</div>} />
-                </Routes>
-        </Suspense>
+          {/* Unplanned Puzzle */}
+          <Route
+          path="/QuickHack" element={
+            <Suspense fallback={<div>Loading QuickHack...</div>}>
+              <QuickHackScreen />
+            </Suspense>
+          }
+          />
+
+          {/* Fallback or home route */}
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
